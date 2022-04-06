566d9f77b823202fc444a75954460246ced2e0c5
Reaz  Tahmidur Rahman <51685766+eram746@users.noreply.github.com>
2022-04-06 13:57:58 -0400

    Update 1.2 Check Premutation.py

--- 1.2 Check Premutation.py
+++ 1.2 Check Premutation.py
@@ -1,12 +1,12 @@
 '''1.2 Check Permutation: Given two strings, write a method to decide if one is a permutation of the 
 other.'''
 
-def Check_Prem(string1, string2):
-    result = 'Is not a premuation'
-    for char in string1:
-        if char not in string2:
-            break
-        else:
-            result = 'Is a premuation'
-    return result
-            
+def Is_prem(string1, string2):
+
+    if len(string1) != len(string2): 
+        return False
+    else: 
+        return sorted(string1) == sorted(string2)
+        #return sorted(string1.lower()) == sorted(string2.lower())
+
+print(Is_prem('college', 'egLleco'))
