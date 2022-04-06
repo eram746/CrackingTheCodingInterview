379511a7684ff6e0ca8015cd43fdc1a8cbeda56c
Reaz  Tahmidur Rahman <51685766+eram746@users.noreply.github.com>
2022-04-06 12:41:55 -0400

    Update 1.1 Is Unique.py

--- 1.1 Is Unique.py
+++ 1.1 Is Unique.py
@@ -1,6 +1,6 @@
-#1.1 Is Unique: Implement an algorithm to determine 
+'''1.1 Is Unique: Implement an algorithm to determine 
 if a string has all unique characters. What if you cannot 
-use additional data structures?
+use additional data structures?'''
 
 def Is_unique(val): 
     unq = True 
